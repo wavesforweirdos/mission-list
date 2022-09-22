0002f9ffb83480331d29482194a2b6f673b9986d
Marta Cruz <inmerset@gmail.com>
2022-09-22 10:10:07 +0200

    simplify routes

--- lib/base/Model.php
+++ lib/base/Model.php
@@ -11,7 +11,7 @@ class Model
 	public function __construct()
 	{
 		// parses the settings file
-		$settings = parse_ini_file(ROOT_PATH . '/config/settings.ini', true);
+		$settings = parse_ini_file(CONFIG_PATH . 'settings.ini', true);
 		
 		// starts the connection to the database
 		$this->_dbh = new PDO(
