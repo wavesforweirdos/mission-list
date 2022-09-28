718d7d7aa0434e37afbf1a9a1e867913dd3eba4b
Marta Cruz <inmerset@gmail.com>
2022-09-28 20:28:50 +0200

    mission functions completed

--- app/controllers/MissionController.php
+++ app/controllers/MissionController.php
@@ -11,17 +11,51 @@ class MissionController extends Controller
 
 		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-			if (isset($_POST['addmission'])) { //añadir mission
-				//redirigir a formulario de newMission
+			if (isset($_POST['addmission'])) {
+				//añadir mission
 				header('Location: newmission');
-
-			} elseif (isset($_POST['logout'])) { //cerrar sesión
-				//añadir boton de logout
-
-			} elseif (isset($_POST['delete'])) {//eliminar misión
-				//recoger el nombre de la mision 
-				$title = $_POST['missionName'];
+			} elseif (isset($_POST['delete'])) {
+				//eliminar misión
+				$title = $_POST['missionName']; //recoger el nombre de la mision desde el input hidden
 				$mission->deleteMission($title);
+				header('Location: mission');
+			} elseif (isset($_POST['complete'])) {
+				//misión completada
+				$title = $_POST['missionName'];
+				$mission->completeMission($title);
+				header('Location: mission');
+			} elseif (isset($_POST['edit'])) {
+				//editar misión
+				$title = $_POST['missionName'];
+				$_SESSION['title'] = $title;
+				header('Location: editmission');
+			} elseif (isset($_POST['starred'])) {
+				//destacar o no la misión
+				$title = $_POST['missionName'];
+				$mission->starredMission($title);
+				header('Location: mission');
+			} elseif (isset($_POST['starredFilter'])) {
+				//filtrar misiones destacadas
+				$starred = 'starred';
+				$_SESSION['missionArray'] = $mission->filterMission($starred);
+			} elseif (isset($_POST['completedFilter'])) {
+				//filtrar misiones completadas
+				$status = 2;
+				$_SESSION['missionArray'] = $mission->filterMission($status);
+			} elseif (isset($_POST['trashedFilter'])) {
+				//filtrar misiones eliminadas
+				$status = 3;
+				$_SESSION['missionArray'] = $mission->filterMission($status);
+			} elseif (isset($_POST['searchMission'])) {
+				//filtrar misiones por nombre
+				$title = 'title';
+				$query = $_POST['searchName'];
+				$_SESSION['searchName'] = $query;
+				$_SESSION['missionArray'] = $mission->filterMission($title);
+			} elseif (isset($_POST['deletemissions'])) {
+				//eliminar todas las misiones de la BD
+				$data = [];
+				$mission->setAllMissions($data);
 				header('Location: mission');
 			}
 		}
@@ -29,27 +63,58 @@ class MissionController extends Controller
 
 	public function newMissionAction()
 	{
-		echo 'Esto es newMissionAction';
+		//echo 'Esto es newMissionAction';
 		$username = $_SESSION['username'];
-		$mission = new Mission($username);
-		$_SESSION['missionArray'] = $mission->getAllMissions();
+		$missions = new Mission($username);
+		$_SESSION['missionArray'] = $missions->getAllMissions();
 
 		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 			if (isset($_POST['newMission']) && !empty($_POST['title']) && !empty($_POST['character']) && !empty($_POST['tag']) && !empty($_POST['end_date'])) {
-
-				//el ususario quiere añadir una mision
+				//el ususario quiere añadir una mision y todos los campos han sido añadidos
 				$title = $_POST['title'];
 				$character = $_POST['character'];
 				$tag = $_POST['tag'];
 				$end_date = $_POST['end_date'];
 
-				$mission->addMission($title, $character, $tag, $end_date);
+				$missions->addMission($title, $character, $tag, $end_date);
 				header('Location: mission');
-			} elseif (isset($_POST['newMission']) && (!empty($_POST['title']) || !empty($_POST['character']) || !empty($_POST['tag']) || !empty($_POST['end_date']))) {
+			} elseif (isset($_POST['newMission']) && (empty($_POST['title']) || empty($_POST['character']) || empty($_POST['tag']) || empty($_POST['end_date']))) {
+				//alguno de los campos no ha sido enviado
 				$error = 'Make sure all the fields are filled in.';
 				$_SESSION['error'] = $error;
 			} elseif (isset($_POST['cancel'])) {
+				//el ususario quiere cancelar la operación
+				header('Location: mission');
+			}
+		}
+	}
 
+	public function editMissionAction()
+	{
+		//echo 'Esto es editMissionAction';
+		$username = $_SESSION['username'];
+		$missions = new Mission($username);
+
+		$title = $_SESSION['title'];
+		$_SESSION['mission'] = $missions->getMission($title);
+		$mission = $_SESSION['mission'];
+
+		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+			if (isset($_POST['editMission'])) {
+				//el ususario quiere cambiar el valor de alguno de los inputs
+				$modified_title = $_POST['title'];
+				$character = $_POST['character'];
+				$tag = $_POST['tag'];
+				$end_date = $_POST['end_date'];
+				$status = $_POST['status'];
+
+				$missions->editMission($title, $modified_title, $character, $tag, $end_date, $status);
+				header('Location: mission');
+			} elseif (isset($_POST['editMission']) && (empty($_POST['title']) || empty($_POST['character']) || empty($_POST['tag']) || empty($_POST['end_date']))) {
+				//alguno de los campos no ha sido enviado
+				$error = 'Make sure all the fields are filled in.';
+				$_SESSION['error'] = $error;
+			} elseif (isset($_POST['cancel'])) {
 				//el ususario quiere cancelar la operación
 				header('Location: mission');
 			}
--- app/models/Mission.php
+++ app/models/Mission.php
@@ -2,6 +2,7 @@
 class Mission
 {
     private $username;
+    /*
     private $title;
     private $character;
     private $tag;
@@ -9,19 +10,18 @@ class Mission
     private $status;
     private $starred;
     private $date_record;
-
+    */
     private $missionArray;
-    private $deletedMissionArray;
-
+    private $filteredArray;
 
     public function __construct($user)
     {
         $this->username = $user;
 
-        if (!file_exists(CONFIG_PATH . '/database/' . $this->username . '/' . $this->username . '-missions.json')) {
-            $missionFile = json_decode(file_put_contents(CONFIG_PATH . '/database/' . $this->username . '/' . $this->username . '-missions.json', '[]'));
+        if (!file_exists(CONFIG_PATH . '/database/' . $this->username . '-missions.json')) {
+            $missionFile = json_decode(file_put_contents(CONFIG_PATH . '/database/' . $this->username . '-missions.json', '[]'));
         } else {
-            $missionFile = json_decode(file_get_contents(CONFIG_PATH . '/database/' . $this->username . '/' . $this->username . '-missions.json'), true);
+            $missionFile = json_decode(file_get_contents(CONFIG_PATH . '/database/' . $this->username . '-missions.json'), true);
         }
         $this->missionArray = $missionFile;
     }
@@ -33,19 +33,41 @@ class Mission
         return $this->missionArray;
     }
 
+    public function getMission($title)
+    {
+        $missionFile = $this->missionArray;
+
+        if (is_array($missionFile)) {
+
+            foreach ($missionFile as $key => $val) {
+
+                if ($val['title'] === $title) {
+                    return $missionFile[$key];
+                }
+            }
+        }
+    }
+
+    //-----------------------SETTERS-----------------------
+    public function setAllMissions($data)
+    {
+        $this->missionArray = $data;
+        return $this->missionArray;
+    }
+
     //añadir mission
     public function addMission($title, $character, $tag, $end_date)
     {
         if (!$this->missionArray) {
             //si no existe un array de misiones, lo crea y añade el usuario
-            $this->missionArray = file_put_contents(CONFIG_PATH . '/database/' . $this->username . '/' . $this->username . '-missions.json', '
+            $this->missionArray = file_put_contents(CONFIG_PATH . '/database/' . $this->username . '-missions.json', '
             { "title":"' . $title . '",
             "character": "' . $character . '",
             "tag": "' . $tag . '",
             "end_date": "' . $end_date . '",
-            "status": "pending",
-            "starred": "0",
-            "date_record": "' . date("Y-m-d H:i:s") . '"
+            "status": 0, 
+            "starred": 0,
+            "date_record": "' . date("Y-m-d") . '"
             }');
         } else {
             if (!$this->CheckMission()) {
@@ -55,14 +77,14 @@ class Mission
                     'character' => $character,
                     'tag' => $tag,
                     'end_date' => $end_date,
-                    'status' => 'pending',
-                    'starred' => '0',
-                    'date_record' => date("Y-m-d H:i:s")
-                ];
+                    'status' => 0, //pending
+                    'starred' => 0, //no starred
+                    'date_record' => date("Y-m-d")
+                ]; //lanzar error si el nombre de la misión ya existe
 
                 $this->missionArray[] = $newMission;
                 $json = json_encode($this->missionArray, JSON_PRETTY_PRINT);
-                file_put_contents(CONFIG_PATH . '/database/' . $this->username . '/' . $this->username . '-missions.json', $json, FILE_APPEND | LOCK_EX);
+                file_put_contents(CONFIG_PATH . '/database/' . $this->username . '-missions.json', $json);
             }
         }
     }
@@ -70,28 +92,140 @@ class Mission
     public function deleteMission($title)
     {
         $missionFile = $this->missionArray;
+
         if (is_array($missionFile)) {
             foreach ($missionFile as $key => $val) {
                 //echo $val['title'];
                 if ($val['title'] === $title) {
 
-                    //añadimos las tareas eliminadas en un archivo aparte
-                    $this->deletedMissionArray[] = $missionFile[$key];
-                    $deletedJson = json_encode($this->deletedMissionArray, JSON_PRETTY_PRINT);
-                    file_put_contents(CONFIG_PATH . 'database/' . $this->username . '/' . $this->username . '-deleted-missions.json', $deletedJson, FILE_APPEND | LOCK_EX);
+                    //cambiamos el statos a 'deleted' == 3
+                    $missionFile[$key]['status'] = 3;
 
-                    //eliminamos la variable del array inicial
-                    unset($missionFile[$key]);
+                    if ($val['status'] != 3) {
+                        $missionFile[$key]['status'] = 3;
+                    } else { //sino lo devolveremos por defecto a pending
+                        $missionFile[$key]['status'] = 0;
+                    }
                 }
             }
+
             $missionFile = json_encode($missionFile, JSON_PRETTY_PRINT);
-            file_put_contents(CONFIG_PATH . '/database/' . $this->username . '/' . $this->username . '-missions.json', $missionFile);
+            file_put_contents(CONFIG_PATH . '/database/' . $this->username . '-missions.json', $missionFile);
+        }
+    }
+
+    public function completeMission($title)
+    {
+        $missionFile = $this->missionArray;
+
+        if (is_array($missionFile)) {
+            foreach ($missionFile as $key => $val) {
+
+                if ($val['title'] === $title) {
+
+                    if ($val['status'] != 2) {
+                        $missionFile[$key]['status'] = 2;
+                        $missionFile[$key]['end_date'] = date("Y-m-d");
+                    } else { //sino lo devolveremos por defecto a pending
+                        $missionFile[$key]['status'] = 0;
+                    }
+                }
+            }
+
+            $missionFile = json_encode($missionFile, JSON_PRETTY_PRINT);
+            file_put_contents(CONFIG_PATH . '/database/' . $this->username . '-missions.json', $missionFile);
+        }
+    }
+
+    public function editMission($title, $modified_title, $character, $tag, $end_date, $status)
+    {
+
+        $missionFile = $this->missionArray;
+
+        if (is_array($missionFile)) {
+            foreach ($missionFile as $key => $val) {
+
+                if ($val['title'] === $title) {
+
+                    //cambiamos los valores por los especificados en los inputs del formulario
+                    $missionFile[$key]['title'] = $modified_title;
+                    $missionFile[$key]['character'] = $character;
+                    $missionFile[$key]['tag'] = $tag;
+                    $missionFile[$key]['end_date'] = $end_date;
+                    $missionFile[$key]['status'] = $status;
+                }
+            }
+
+            $missionFile = json_encode($missionFile, JSON_PRETTY_PRINT);
+            file_put_contents(CONFIG_PATH . '/database/' . $this->username . '-missions.json', $missionFile);
+        }
+    }
+
+    public function starredMission($title)
+    {
+        $missionFile = $this->missionArray;
+
+        if (is_array($missionFile)) {
+            foreach ($missionFile as $key => $val) {
+
+                if ($val['title'] === $title) {
+
+                    //cambiamos starred al valor contrario 
+                    if ($val['starred'] == 0) {
+                        $missionFile[$key]['starred'] = 1;
+                    } else {
+                        $missionFile[$key]['starred'] = 0;
+                    }
+                }
+            }
+
+            $missionFile = json_encode($missionFile, JSON_PRETTY_PRINT);
+            file_put_contents(CONFIG_PATH . '/database/' . $this->username . '-missions.json', $missionFile);
+        }
+    }
+
+    public function filterMission($filter)
+    {
+        $missionFile = $this->missionArray;
+
+        //array_filter — Filtra elementos de un array usando una función de devolución de llamada (array, funcion, filtro)
+
+        if (is_array($missionFile)) {
+            if ($filter === 'starred') {
+                $query = 1;
+                $arrayFiltered = array_filter($missionFile, function ($var) use ($query) {
+                    return ($var['starred'] == $query);
+                });
+            } elseif ($filter === 'title') {
+                $arrayFiltered = [];
+                $query = strtolower($_SESSION['searchName']);
+
+                foreach ($missionFile as $key => $val) {
+                    // echo $val['title'];
+
+                    // echo '<pre>';
+                    // print_r($missionFile[$key]);
+                    // echo '</pre>';
+
+                    if (strpos(strtolower($val['title']), $query) !== false) {
+                        array_push($arrayFiltered, $missionFile[$key]);
+                    }
+                }
+                return $arrayFiltered;
+            } else {
+                $query = $filter;
+                $arrayFiltered = array_filter($missionFile, function ($var) use ($query) {
+                    return ($var['status'] == $query);
+                });
+            }
+            return $this->missionArray = $arrayFiltered;
         }
     }
 
 
     public function CheckMission()
     {
+
         if (!$this->missionArray) {
             return false;
         } else {
